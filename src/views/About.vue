f8fc093f35856292a3699e436c333cff11d518dd
jorntx <jt@kb.dk>
2020-11-12 16:04:08 +0100

    updated about text

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,12 +4,11 @@
       Freetext search in the Danish Netarchive
     </h1>
     <p>
-      Since 2004 The Danish Net Archive has been harvesting the Danish part of the internet multiple times every year. For the period 1998 to 2004 we only have sparsely fragments of the Danish internet and data are not representative for these years. The freetext search will search in html-pages for each year and the number of results found will be compared
-      to the total number of html from that year.
+      Since 2004 The Danish Net Archive has been harvesting the Danish part of the internet multiple times every year. For the period 1998 to 2004 we only have sparsely fragments of the Danish internet and data are not representative for these years. The freetext search will search in html-pages for each year and the number of results found will be compared to the total number of html from that year. The total number of html pages in the archive is currently 20 billion (20,000,000,000).
     </p>
 
     <p class="legalDisclaimer">
-      Due to legal reasons only one or two words can be used in the search.
+      Due to legal reasons only one or two words can be used in the search and no special characters allowed.
     </p>
 
     <p>For more information about the Danish Netarchive see <a href="http://netarkivet.dk/in-english/" target="_new">Danish Netarchive</a>.</p>
