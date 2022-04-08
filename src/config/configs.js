d72455f69f2e7ae528509eed8889b8b36e20c0a5
jorntx <jt@kb.dk>
2022-04-08 12:59:48 +0200

    Fix wrong end date in graph

--- src/config/configs.js
+++ src/config/configs.js
@@ -1,5 +1,5 @@
 export default {
-  SERVICE_URL : '/tags-service/smurf/netarchive/text',
-  START_YEAR : 1998,
-  END_YEAR: 2021
+  SERVICE_URL: '/tags-service/smurf/netarchive/text',
+  START_YEAR: 1998,
+  END_YEAR: (new Date().getFullYear() + 1).toString()
 }
